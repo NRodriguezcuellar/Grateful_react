2f7be0a09571db70a9b3a511711844343e6520b0
Nrodriguezcuellar <nicolas.rodriguezcuellar@student.hu.nl>
2021-04-22 21:08:49 +0200

    added functionality to add labels

--- src/view/AddMomentView.tsx
+++ src/view/AddMomentView.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import {IonInput, IonItem, IonLabel, IonRange, IonTextarea, IonIcon, IonChip, IonButton} from "@ionic/react";
+import {IonInput, IonItem, IonLabel, IonRange, IonTextarea, IonIcon, IonChip, IonButton, IonItemGroup} from "@ionic/react";
 import {happyOutline, sadOutline, addOutline} from 'ionicons/icons';
 import {useState} from "react";
 
@@ -13,12 +13,25 @@ const InputItem = styled(IonItem)``
 const Title = styled.h1` text-align: center;
   margin-bottom: 2rem`
 
-const LabelContainer = styled(IonItem)`display: flex`
+const LabelContainer = styled(IonItem)`display: flex; flex-direction: column;`
+
+const LabelTitle = { paddingBottom: "10px"}
+
+const hidden = {visibility: "hidden"}
 
 
 const AddMomentView: React.FC = () => {
 
-    const labels = useState<string[]>([])
+    const [labels, setLabels] = useState<string[]>([])
+    const [labelInput, setLabelInput] = useState('')
+
+    const labelHandler = (label: string) => {
+        const labelAlreadyExists = labels.find(savedLabel => savedLabel === label )
+        if(labelAlreadyExists) return;
+
+
+        setLabels([...labels, label])
+    }
 
 
     return <ParentDiv>
@@ -40,11 +53,32 @@ const AddMomentView: React.FC = () => {
             </IonRange>
         </InputItem>
 
+
+
+            <IonItem lines="full">
+                <IonInput placeholder="A label" onIonChange={(event) => setLabelInput(event.detail.value!.toString())}/>
+                <IonButton onClick={ () => labelHandler(labelInput)}>
+                    <IonIcon slot="end" icon={addOutline}/>
+                    Add label
+                </IonButton>
+            </IonItem>
+
+
+
+
+
+
         <LabelContainer lines="full">
-            <IonButton>
-                <IonIcon slot="end" icon={addOutline}/>
-                Add label
-            </IonButton>
+            <IonLabel position="stacked" style={LabelTitle}>Labels</IonLabel>
+
+
+
+            <div>
+                {
+                    labels.map((label, index) => <IonChip key={index}>{label}</IonChip>)
+                }
+                <IonChip style={hidden}/>
+            </div>
 
         </LabelContainer>
 
